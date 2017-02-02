a42e78e5cb63bc102f42c358e35400be93a60381
Unknown <dkitic@croz.net>
2017-02-02 16:08:22 +0100

    Add osoba

---
+++ src/Osoba.groovy
@@ -0,0 +1,14 @@
+import groovy.transform.PackageScope
+
+/**
+ * Created by darko on 02-Feb-17.
+ */
+class Osoba {
+
+    @PackageScope
+    def ime = "darko"
+    def prezime = " kitic"
+    def bojaKose = " smedja"
+
+
+}
