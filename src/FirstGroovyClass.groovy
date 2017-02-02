3004efbfa41ceb61ea45e9a0e5c8f4bc8cd713a8
Unknown <dkitic@croz.net>
2017-02-02 15:31:33 +0100

    Commit on darko branch

--- src/FirstGroovyClass.groovy
+++ src/FirstGroovyClass.groovy
@@ -3,6 +3,11 @@
  */
 class FirstGroovyClass {
 
-    
+    public static void main(String[] args) {
+
+        //  System.out.println("hello groovy")
+        System.out.println("hello world")
+        println 'groovy primjer'
+    }
 
 }
